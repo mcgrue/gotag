745d177efc82dc1d1678784eb46b4b982b80b6af
Ben McGraw <>
2025-03-11 16:04:17 -0700

    can do 'fake tagging' now

--- src/client/main.go
+++ src/client/main.go
@@ -1,47 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"io"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "pingpawn.com/gotag/protos/helloworld"
-)
-
-const (
-	defaultName = "world"
+	pb "pingpawn.com/gotag/protos/tagger"
 )
 
 var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
 )
+
+func readStdin() string {
+	reader := bufio.NewReader(os.Stdin)
+	var content string
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			content += line
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error reading stdin: %v", err)
+		}
+		content += line
+	}
+	return content
+}
 
 func main() {
 	flag.Parse()
+
+	// Read content from stdin
+	content := readStdin()
+
+	// print the content
+	log.Printf("Content: %s", content)
+
 	// Set up a connection to the server.
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+	c := pb.NewTaggerClient(conn)
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	r, err := c.TagText(ctx, &pb.UnstructuredText{UnstructuredEntry: content})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
-
-	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: *name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	log.Printf("Tags: %s", strings.Join(r.GetTags(), ", "))
 }
--- src/server/main.go
+++ src/server/main.go
@@ -5,30 +5,57 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 
 	"google.golang.org/grpc"
-	pb "pingpawn.com/gotag/protos/helloworld"
+	// pb "pingpawn.com/gotag/protos/helloworld"
+	pb "pingpawn.com/gotag/protos/tagger"
 )
 
 var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
+var randomWords = [10]string{
+	"cat",
+	"sun",
+	"blue",
+	"jump",
+	"quick",
+	"fox",
+	"lazy",
+	"dog",
+	"moon",
+	"tree",
+}
+
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.TaggerServer
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
+// implements tagger.TagText
+func (s *server) TagText(_ context.Context, in *pb.UnstructuredText) (*pb.TagReply, error) {
+	log.Printf("Received: %v", in.GetUnstructuredEntry())
 
-func (s *server) SayHelloAgain(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello again " + in.GetName()}, nil
+	log.Printf("Received: %v", in.GetUnstructuredEntry())
+
+	// Shuffle the randomWords slice
+	for i := len(randomWords) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		randomWords[i], randomWords[j] = randomWords[j], randomWords[i]
+	}
+
+	// Slice 2-5 entries from the shuffled slice
+	numTags := rand.Intn(4) + 2 // Random number between 2 and 5
+	selectedTags := randomWords[:numTags]
+
+	// Create the TagReply with the selected tags
+	reply := &pb.TagReply{
+		Tags: selectedTags,
+	}
+	return &pb.TagReply{Tags: reply.Tags}, nil
 }
 
 func main() {
@@ -38,7 +65,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterTaggerServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
